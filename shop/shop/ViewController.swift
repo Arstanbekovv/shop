5a492a35ff04b2a6f315b36befe40046f9d286c6
Arstanbekovv <arstanbekov.a.t@gmail.com>
2022-05-31 20:24:09 +0600

    init add

---
+++ shop/shop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  shop
+//
+//  Created by Aziz on 31/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
